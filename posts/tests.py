c0c174b40fa7ae8e24b2133753fce1714cbdbdf2
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2025-07-02 12:54:33 +0200

    posts app setup with models & tests

---
+++ posts/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from .models import Post
+
+# Create your tests here.
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(title="Test Post", body="This is a test post.")
+
+    def test_post_creation(self):
+        post = Post.objects.get(title="Test Post")
+        self.assertEqual(post.body, "This is a test post.")
+
+    def test_post_str(self):
+        post = Post.objects.get(title="Test Post")
+        self.assertEqual(str(post), "Test Post")
+    
+    def test_post_created_auto_now_add(self):
+        post = Post.objects.get(title="Test Post")
+        self.assertIsNotNone(post.created_at)
+
+    def test_post_ordering(self):
+        posts = Post.objects.all()
+        self.assertEqual(posts[0].title, "Test Post")  # Assuming this is the only post
+        
+        
